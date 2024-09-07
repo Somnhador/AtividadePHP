41c27c4edef2e6194916f3c4c9e7df9931364c71
Gabriel BS <178868428+Somnhador@users.noreply.github.com>
2024-09-07 00:35:02 -0300

    att2

--- atividade6.php
+++ atividade6.php
@@ -6,51 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>ATIVIDADE 3</title>
+    <title>ATIVIDADE 6</title>
 </head>
 
 <body>
     <?php
-    $valor1 = $_GET['v1'] ?? null;
-    $valor2 = $_GET['v2'] ?? null;
-    $calcular = $_GET['calcular'] ?? null;
+    $numero = $_GET['numero'] ?? null;
     ?>
     <main>
-        <h1>CALCULADORA BÁSICA</h1><br>
+        <h1>PAR OU ÍMPAR</h1><br>
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
-            <label for="v1">Valor 1</label>
-            <input type="number" name="v1" id="v1" value="<?= $valor1 ?>"><br><br>
-            <label for="v2">Valor 2</label>
-            <input type="number" name="v2" id="v2" value="<?= $valor2 ?>"><br><br>
-
-            <button type="submit" name=calcular value="somar">SOMAR</button>
-            <button type="submit" name=calcular value="subtrair">SUBTRAIR</button>
-            <button type="submit" name=calcular value="multiplicar">MULTIPLICAR</button>
-            <button type="submit" name=calcular value="dividir">DIVIDIR</button>
+            <label for="numero">Insira o número</label>
+            <input type="number" name="numero">
+            <button type="submit">Verificar</button>
         </form>
     </main>
 
-    <?php if ($valor1 !== null && $valor2 !== null && $calcular !== null): ?>
+    <?php if ($numero !== null): ?>
         <section id="resultado">
-            <h2>RESULTADO DO CÁLCULO</h2>
+            <h2>RESULTADO</h2>
             <?php
-            switch ($calcular) {
-                case 'somar':
-                    $calculo = $valor1 + $valor2;
-                    echo "<p>A soma entre o número $valor1 e o número $valor2 é igual a $calculo</p>";
-                    break;
-                case 'subtrair':
-                    $calculo = $valor1 - $valor2;
-                    echo "<p>A subtração entre o número $valor1 e o número $valor2 é igual a $calculo</p>";
-                    break;
-                case 'multiplicar':
-                    $calculo = $valor1 * $valor2;
-                    echo "<p>A multiplicação entre o número $valor1 e o número $valor2 é igual a $calculo</p>";
-                    break;
-                case 'dividir':
-                    $calculo = $valor1 / $valor2;
-                    echo "<p>A divisão entre o número $valor1 e o número $valor2 é igual a $calculo</p>";
-                    break;
+            $numero = $_GET['numero'];
+
+            if ($numero % 2 == 0) {
+                echo "$numero é Par!";
+            } else {
+                echo "$numero é Ímpar!";
             }
             ?>
         </section>
